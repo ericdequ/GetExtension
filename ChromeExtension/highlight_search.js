0d0a668336955f0ec734df65d4f08e6b7ba66bb2
Angel Lopez Pol <59091763+angel1254mc@users.noreply.github.com>
2022-08-17 16:18:55 -0400

    added index cuz i forgor

--- ChromeExtension/highlight_search.js
+++ ChromeExtension/highlight_search.js
@@ -33,7 +33,7 @@ let getText = (e) => {
 const handleRemoveFakePopup = () => {
   current_page = null;
   currentItems = [];
-  document.getElementsByClassName('fake-popup-container').innerHTML= "";
+  document.getElementsByClassName('fake-popup-container')[0].innerHTML= "";
 }
 const navigateDefinitionsPopup = (term) => {
     //animate out contents and animate in new contents
